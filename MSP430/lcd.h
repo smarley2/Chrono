c37881e06931cc3112e33434282ae1719d64591b
smarley2 <fabiocposser@gmail.com>
2016-02-16 07:50:35 -0200

    Removido linhas do .h que estavam no .C Adicionado código em hexa para comandos do display.

--- MSP430/lcd.h
+++ MSP430/lcd.h
@@ -14,6 +14,33 @@ P1.5 - R/W Pin5
 P1.6 - E Pin6
 */
 
+/*
+CodeHex) Command to LCD Instruction Register
+
+1        Clear display screen
+2        Return Home
+4        Decrement cursor (shift cursor to left)
+5        Increment cursor (shift cursor to right)
+6        shift display right
+7        shift display left
+8        Display off, cursor off
+A        Display off, cursor on
+C        Display on, cursor off
+E        Display on, cursor blinking
+F        Display on, cursor blinking
+10        Shift cursor position to left
+14        Shift cursor position to right
+18        Shift the entire display to the left
+1C        Shift the entire display to the right
+80        Force cursor to the beginning of 1st line
+C0        Force cursor to the beginning of 2nd line
+38        2 lines and 5 x 7 matrix
+
+For displaying the characters on the 1st line send the command 0x80
+For displaying the characters on the 2nd line send the command 0xC0
+For displaying the characters on the 3rd line send the command 0x94
+For displaying the characters on the 4th line send the command 0xD4
+*/
 //#include <msp430g2553.h>
 #include <msp430.h>
 #define DR P1OUT = P1OUT | BIT4 // define RS high
