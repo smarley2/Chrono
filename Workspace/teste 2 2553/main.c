dd32bf716b9fa06728c8ba465d751c051f4a54ff
smarley2 <fabiocposser@gmail.com>
2016-02-17 15:40:51 -0200

    Adicionado comandos do LCD, função para medir temperatura do Chip. Validado com osciloscópio a medição do timer e funcionamento das interrupções nos io's.

--- Workspace/teste 2 2553/main.c
+++ Workspace/teste 2 2553/main.c
@@ -1,28 +1,14 @@
-#define TINYPRINTF_DEFINE_TFP_PRINTF 1
-#define TINYPRINTF_DEFINE_TFP_SPRINTF 0
-#define TINYPRINTF_OVERRIDE_LIBC 0
-
-#include <stdio.h>
-#include <string.h>
-#include <limits.h>
-
 #include <msp430.h>
 #include <lcd.h>
-#include <tinyprintf.h>
-
-/* Clear unused warnings for actually unused variables */
-#define UNUSED(x) (void)(x)
-
-#define TPRINTF(expr...) \
-  ({ printf("libc_printf(%s) -> ", #expr); printf(expr); \
-     printf(" tfp_printf(%s) -> ", #expr); tfp_printf(expr); })
-
 
 unsigned short ponto2 = 0;
 unsigned short volatile estado = 0;
 unsigned long vel_ms = 0, vel_fps = 0;
+volatile int temp = 0;
 
 void pin_init();
+void tempInit();
+int tempOut();
 
 int main(void)
 {
@@ -35,16 +21,19 @@ int main(void)
   }
   DCOCTL = 0;                               // Select lowest DCOx and MODx settings
   BCSCTL1 = CALBC1_16MHZ;                   // Set range
+  BCSCTL2 = SELM_0 + DIVM_0 + DIVS_0;                   // Set range
   DCOCTL = CALDCO_16MHZ;                    // Set DCO step + modulation
 
-  BCSCTL1 = DIVA_0;
+/*  BCSCTL1 |= DIVA_0;
+  BCSCTL1 &= ~XT2OFF;
   BCSCTL2 = SELM_2 + DIVM_0 + SELS + DIVS_0;
-  BCSCTL3 = XT2S_2 + XCAP_3;
-
+  BCSCTL3 = XT2S_2 + XT2S1 + XCAP_3;
+*/
   pin_init();
   lcd_init();
+  tempInit();//initialise adc
 
-  TACTL = TASSEL_2 + MC_2;           // SMCLK, contmode
+  TACTL = ID_0 + TASSEL_2 + MC_2;           // SMCLK, contmode
 
   __bis_SR_register(GIE);
 
@@ -54,11 +43,25 @@ int main(void)
 	  {
 //		  vel_ms = 0.5 / (ponto2 * (1/16000000));
 //		  vel_fps = vel_ms / 0.3048;
+//		  ponto2 = 26246; //utilizado para teste
+
 		  vel_ms = 8000000 / ponto2; // 1/f * distância / clks
 		  vel_fps = 26246719 / ponto2; // 1/feet * 1/f * distância / clks
-
+//		  send_data(0x01);
+		  send_command(0x0E);
+		  send_command(0x80);
 		  send_string("Vel = ");
+		  integerToLcd(vel_ms);
+		  send_string(" m/s");
 		  send_command(0xC0);
+		  send_string("Vel = ");
+		  integerToLcd(vel_fps);
+		  send_string(" fps");
+		  temp=tempOut();
+		  send_command(0x94);
+		  send_string("Temp = ");
+		  integerToLcd(temp);
+		  send_string(" °C");
 
 		  estado = 0;
 	  }
@@ -70,7 +73,7 @@ int main(void)
 #pragma vector=PORT2_VECTOR
 __interrupt void Port_2(void)
 {
-	if((P2IFG & 0x8) && (estado == 0)) //Interrupção pino 2.3, ponto1
+	if((P2IFG & 0x08) && (estado == 0)) //Interrupção pino 2.3, ponto1
 	{
 		TA0R = 0;
 		estado = 1;
@@ -106,9 +109,54 @@ PxIN can then be read
 
 	  //Configuração das entradas
 	  P2OUT =  0x18;                            // P2.3 and P2.4 set, else reset
-	  P2REN |= 0x18;                            // P2.3 and P2.4 pullup
+//	  P2REN |= 0x18;                            // P2.3 and P2.4 pullup
 	  P2IE |= 0x18;                             // P2.3 and P2.4 interrupt enabled
 	  P2IES &= 0x00;                            // P2.3 and P2.4 Lo/Hi edge
 	  P2IFG &= ~0x18;                           // P2.3 and P2.4 IFG cleared
       ///////////////////////////////////////
+}
+
+void tempInit()
+{
+    ADC10CTL0=SREF_1 + REFON + ADC10ON + ADC10SHT_3 ; //1.5V ref,Ref on,64 clocks for sample
+    ADC10CTL1=INCH_10+ ADC10DIV_3; //temp sensor is at 10 and clock/4
+}
+int tempOut()
+{
+    int t=0;
+    __delay_cycles(1000);              //wait 4 ref to settle
+    ADC10CTL0 |= ENC + ADC10SC;      //enable conversion and start conversion
+    while(ADC10CTL1 & BUSY);         //wait..i am converting..pum..pum..
+    t=ADC10MEM;                       //store val in t    ADC10CTL0 |= ENC + ADC10SC;
+    ADC10CTL0 |= ADC10SC;
+    while(ADC10CTL1 & BUSY);
+    t+=ADC10MEM;
+    ADC10CTL0 |= ADC10SC;
+    while(ADC10CTL1 & BUSY);
+    t+=ADC10MEM;
+    ADC10CTL0 |= ADC10SC;
+    while(ADC10CTL1 & BUSY);
+    t+=ADC10MEM;
+    ADC10CTL0 |= ADC10SC;
+    while(ADC10CTL1 & BUSY);
+    t+=ADC10MEM;
+    ADC10CTL0 |= ADC10SC;
+    while(ADC10CTL1 & BUSY);
+    t+=ADC10MEM;
+    ADC10CTL0 |= ADC10SC;
+    while(ADC10CTL1 & BUSY);
+    t+=ADC10MEM;
+    ADC10CTL0 |= ADC10SC;
+    while(ADC10CTL1 & BUSY);
+    t+=ADC10MEM;
+    ADC10CTL0 |= ADC10SC;
+    while(ADC10CTL1 & BUSY);
+    t+=ADC10MEM;
+    ADC10CTL0 |= ADC10SC;
+    while(ADC10CTL1 & BUSY);
+    t+=ADC10MEM;
+    t = t / 10;
+
+    ADC10CTL0&=~ENC;                     //disable adc conv
+    return(int) ((t * 27069L - 18169625L) >> 16); //convert and pass
 }
